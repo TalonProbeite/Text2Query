b6a9c7bcf14e25ff198096b56ba76744b4e0b522
artem ponomarcev <siionunstoppable@gmail.com>
2026-03-22 22:39:06 +0300

    chore: add base config for app

---
+++ backend/app/core/config.py
@@ -0,0 +1,28 @@
+from  pydantic_settings import BaseSettings , SettingsConfigDict
+from typing import List
+from pathlib import Path 
+
+class Settings(BaseSettings):
+
+    app_name: str = "SQLCraft"
+    debug: bool = True
+
+    database_url: str = "postgresql://user:pass@localhost/sqlcraft"
+
+    cors_origins: List[str] = [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:3000",
+    ]
+
+    BASE_DIR: Path = Path(__file__).resolve().parent.parent.parent.parent
+    static_dir: Path =  BASE_DIR / "frontend"
+   
+
+    model_config = SettingsConfigDict(
+    env_file=BASE_DIR / ".env",
+    extra="ignore"
+)
+
+settings = Settings()
